20e7dd42765f67f239705abfa77faf0fb53e391b
VE3ZRD <ve3zrd@gmail.com>
2024-04-23 07:54:06 -0400

    updated

--- gitbeta.sh
+++ gitbeta.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #########################################################
-#  Nextion TFT Support for Nextion 2.4 & 3.5" 		#
+#  Nextion TFT Support for Nextion 3.5" 		#
 #  Gets all Scripts and support files from github       #
-#  and copies them into the Nextion_Support directory   "
+#  and copies them into the Nextion_Support directory   #
 #  and copies the NX??? tft file into /usr/local/etc    #
 #  and returns a script duration time to the Screen 	#
 #  as a script completion flag				#
 #							#
-#  KF6S/VE3RD                               2020-05-12  #
+#  VE3RD                                    2024-04-23  #
 #########################################################
-# Valid Screen Names for VE3ZRD - NX3224K024, NX4832K035
-# Valid Screen Names for VE3RD - NX3224K024
+# Valid Screen Names for VE3ZRD - NX4832K035
 
 #if [[ $EUID -ne 0 ]]; then
 #	clear
@@ -83,10 +82,6 @@ if [ -d "$homedir"/Nextion_Temp ]; then
   	sudo rm -R "$homedir"/Nextion_Temp
 fi
 
-    	if [ "$scn" == "NX3224K024" ]; then
-	  	sudo git clone --depth 1 https://github.com/VE3ZRD/NX3224K024 "$homedir"/Nextion_Temp
-		tst=1
-	fi     
 	if [ "$scn" == "NX4832K035" ]; then
 	  	sudo git clone --depth 1 https://github.com/VE3ZRD/NX4832K035-KDO "$homedir"/Nextion_Temp
 		tst=2
@@ -94,122 +89,21 @@ fi
 	
 }
 
-# VE3RD Script Function
-function getve3rd
-{
-if [ -d "$homedir"/Nextion_Temp ]; then
-  	sudo rm -R "$homedir"/Nextion_Temp
-fi
-	tst=0
-#	echo "Function VE3RD"
-     	
-	calltxt="VE3RD"
-	if [ "$scn" = "NX3224K024" ]; then	
-	 	tst=1  
-	  	sudo git clone --depth 1 https://github.com/VE3RD/Nextion "$homedir"/Nextion_Temp
-	elif [ "$scn" == "NX4832K035" ]; then
-	  	sudo git clone --depth 1 https://github.com/VE3RD/NX4832K035 "$homedir"/Nextion_Temp
-		tst=2
-	else
-		errtext="Invalid VE3RD Screen Name $scn,  $s1,  $s2"
-		exitcode 
-	fi
-
-}
-
-function getcall
-{
-#Set Screen Author
-calltxt=""
-if [ "$parm2" == VE3RD ] || [ "$parm1" == VE3RD ] ; then
-	calltxt="VE3RD"
-else
-	calltxt="VE3ZRD"
-fi
-}
-
 
 #### Start of Main Code
 
 ## Select User Screens
-getcall
 S1=""
 S2=""
-if [ -f "/usr/local/etc/NX4832K035.tft" ]; then
-   S1="NX4832K035"
-   S1A=" Available     "
-else 
-   S1="NX4832K035"
-   S1A=" Not Available "
-fi
-if [ -f "/usr/local/etc/NX3224K024.tft" ]; then
-   S2="NX3224K024"
-   S2A=" Available     "
-else
-   S2="NX3224K024"
-   S2A=" Not Available "
-fi
-
-
-result=(dialog --backtitle "Screen Selector - $calltxt" --ascii-lines --menu "Choose Your $calltxt Nextion Screen Model" 22 76 16)
-
-options=(1 "$S1A 3.5 Inch Nextion Screen"
-         2 "$S2A 2.4 Inch Nextion Screen"
-         3 " Abort - Exit Script")
-
-choices=$("${result[@]}" "${options[@]}" 2>&1 >/dev/tty)
-
-#errt="$?"
-clear
-echo "Choice = $choices"
-
-if [ -z "$choices" ]; then
-#if [ "$choices" != "1" ] || [ "$choices" != "2" ] || [ "$choices" != "3" ]; then
-  errtext="Cancel Button Pressed"
-  exitcode
-fi
-
-for choice in $choices
-do
-    case $choice in
-        1)
-            echo "$S1A 3.5 Inch Nextion Screen Selected"
-		scn="NX4832K035"
-            ;;
-        2)
-            echo "$S2A 2.4 Inch Nextion Screen Selected"
-		scn="NX3224K024"
-            ;;
-        3)
-            echo "Abort - Exit Script"
-		errtext="Abort Selected"
-		exitcode
-            ;;
-    esac
-done
-
-
-if [ "$calltxt" == "VE3RD" ]; then
-	if [ "$result" == "NX3224K024" ]; then
-#echo "Trap2"
-		scn="$result"
-	else
-#echo "Trap3"
-		errtext=" Invalid  Screen name for $calltxt"
-	fi
-fi
-
-echo "$scn $calltxt"
-
-
-#echo " End Processing Parameters  - $scn $calltxt"
+scn="NX4832K035"
+calltxt="VE3ZRD"
 
 #Start Duration Timer
 start=$(date +%s.%N)
 
 model="$scn"
 tft='.tft' 
-#gz='.gz'
+
 #Put Pi-Star file system in RW mode
 sudo mount -o remount,rw / > /dev/null
 sleep 1s
@@ -227,18 +121,8 @@ fi
   # Get Nextion Screen/Scripts and support files from github
   # Get VE3ZRD File Set
 
-if [ "$calltxt" == "VE3ZRD" ]; then
-	echo "getting Screens for $calltxt"
-	getVE3ZRD
+getVE3ZRD
  
-fi
-
-
-  # Get VE3RD File Set
-if [ "$calltxt" == "VE3RD" ]; then
-	echo "Getting Screens for $calltxt"
-	getve3rd
-fi
 
 
 if [ ! -d /usr/local/etc/Nextion_Support ]; then
