dd2e2d9e3b912e0cb1dfca196681f2fb4b216fc0
VE3ZRD <ve3zrd@gmail.com>
2024-04-24 22:01:33 -0400

    update ve3rd

--- gitbeta.sh
+++ gitbeta.sh
@@ -1,169 +1,165 @@
 #!/bin/bash
 #########################################################
-#  Nextion TFT Support for Nextion 3.5" 		#
+#  Nextion TFT Support for Nextion 2.4" 		#
 #  Gets all Scripts and support files from github       #
-#  and copies them into the Nextion_Support directory   #
+#  and copies them into the Nextion_Support directory   "
 #  and copies the NX??? tft file into /usr/local/etc    #
 #  and returns a script duration time to the Screen 	#
 #  as a script completion flag				#
 #							#
-#  VE3RD                                    2024-04-23  #
+#  KF6S/VE3RD                               2021-12-21  #
 #########################################################
-# Valid Screen Names for VE3ZRD - NX4832K035
-
-#if [[ $EUID -ne 0 ]]; then
-#	clear
-#	echo ""
- #  	echo "This script must be run as root"
-#	echo "Setting root user"
-#	echo "Re-Start Script"
-#	echo ""
-#	sudo su
-  # 	exit 1
-#fi
-p1=$(pwd) ; cd .. ; homedir=$(pwd) ; cd "$p1"
-
-who=$(whoami)
-echo "This script is running as $who user"
-sleep 2
-
-run=""
-
-errtext="This is a test"
-
-
-parm1="$1"
-parm2="$2"
-ver="20220124"
+# Use screen model from command $1
+# Valid Screen Names for EA7KDO - NX3224K024, NX4832K935
 declare -i tst
 
-export NCURSES_NO_UTF8_ACS=1
-export LANG=en_US.UTF-8
-
-if [ ! -f ~/.dialog ]; then
-# j=1
-# else
- sudo dialog --create-rc ~/.dialogrc
-fi
-
-sudo sed -i '/use_colors = /c\use_colors = ON' ~/.dialogrc
-sudo sed -i '/screen_color = /c\screen_color = (WHITE,BLUE,ON)' ~/.dialogrc
-sudo sed -i '/title_color = /c\title_color = (YELLOW,RED,ON)' ~/.dialogrc
-echo -e '\e[1;44m'
-
-if [ -z "$1" ]; then
+if [ -z "$0" ]; then
 	clear
+	echo ""
+	echo "  No Screen Name Provided"
+	echo
+        echo "	 Valid Screens - EA7KDO) - NX3224K024, NX4832K035"
+	echo " " 
+	echo " 	Syntax: gitcopy.sh NX????K???   // Will copy EA7KDO Files - Default for Screen"
+	echo " 	Adding a second parameter(anything) will provide feedback as the script runs (Commandline)"
+	echo " "
+	exit
 fi
+ p1=$(pwd) ; cd .. ; homedir=$(pwd) ; cd "$p1"
 
+#s1="NX3224K024"
+s3="NX4832K035"
+errtext="Error! - Aborting"
+
+## Strip off .tft - Take only the first 10 characters
+scn=$(echo "$s3" | tr [:lower:] [:upper:])
+#tr [:lower:] [:upper:]
+scn="${scn:0:10}"
+
+#Feed back is off by default - Turn it on with anything as parameter  2
+fb="$2"
+#Set Call to EA7KDO
+calltxt="EA7KDO"
+
+#echo "$scn"
+
+## Programmed Shutdown
 function exitcode
 {
-txt='Abort Function\n\n
-This Script will Now Stop'"\n$errtext"
-
-dialog --title "  Programmed Exit  " --ascii-lines --msgbox "$txt" 8 78
-
-clear
-echo -e '\e[1;40m'
-run="Done"
-exit
+	echo "Script Execution Failed "
+	echo "$scn"
+	echo "$errtext"
+	exit
 
 }
 
-
-
-# VE3ZRD Script Function
-function getVE3ZRD
+function cleandirs()
 {
-	tst=0
-#	echo "Function VE3ZRD"
-	calltxt="VE3ZRD"
-
+if [ -d /usr/local/etc/Nextion_Support ]; then
+    sudo rm -R /usr/local/etc/Nextion_Support
+fi
 if [ -d "$homedir"/Nextion_Temp ]; then
-  	sudo rm -R "$homedir"/Nextion_Temp
+    sudo rm -R "$homedir"/Nextion_Temp
+fi
+if [ -f /usr/local/etc/"$model$tft" ]; then
+	sudo rm /usr/local/etc/NX*.tft
+fi
+if [ "$fb" ]; then
+    echo "Removed /usr/local/etc/Nextion_Support Directory"
+    echo "Removed "$homedir"/Nextion_Temp Directory"
+    echo "Remove Existing $model$tft"
 fi
 
+}
+
+# EA7KDO Script Function
+function getea7kdo
+{
+	tst=0
+#	echo "Function EA7KDO"
+	calltxt="EA7KDO"
+
+    	if [ "$scn" == "NX3224K024" ]; then
+		cleandirs
+	  	sudo git clone --depth 1 https://github.com/EA7KDO/NX3224K024 "$homedir"/Nextion_Temp
+		chmod +x "$homedir"/Nextion_Temp/*.sh
+		mkdir /usr/local/etc/Nextion_Support
+		sudo rsync -avqru "$homedir"/Nextion_Temp/* /usr/local/etc/Nextion_Support/ --exclude=NX* 
+		sudo cp "$homedir"/Nextion_Temp/"$model$tft" /usr/local/etc/
+		if [ "$fb" ]; then
+		    	echo "Downloaded new Screen package for $model$tft"
+			echo "Copied new tft to /usr/local/etc/"	
+		fi
+tst=1		
+	fi     
 	if [ "$scn" == "NX4832K035" ]; then
+		cleandirs
 	  	sudo git clone --depth 1 https://github.com/VE3ZRD/NX4832K035-KDO "$homedir"/Nextion_Temp
-		tst=2
+		sudo chmod +x "$homedir"/Nextion_Temp/*.sh
+		mkdir /usr/local/etc/Nextion_Support
+		sudo rsync -avqru "$homedir"/Nextion_Temp/* /usr/local/etc/Nextion_Support/ --exclude=NX* 
+		sudo cp "$homedir"/Nextion_Temp/"$model$tft" /usr/local/etc/
+		if [ "$fb" ]; then
+		    	echo "Downloaded new Screen package for $model$tft"
+			echo "Copied new tft to /usr/local/etc/"	
+		fi
      	fi
-	
+tst=2	
+	if [ "$tst" == 0 ]; then
+		errtext="Invalid EA7KDO Screen Name $scn"	
+		exitcode 
+	fi
 }
 
 
 #### Start of Main Code
 
-## Select User Screens
-S1=""
-S2=""
-scn="NX4832K035"
-calltxt="VE3ZRD"
+
+#echo "$scn  - $call" 
+if [ "$fb" ]; then
+	        if [ "$scn" != "$s1" -a "$scn" != "$s3" ]; then
+              		echo "EA7KDO Screen Name MUST be NX3224K024 or NX4832K035"
+                       	errtext="Invalid EA7KDO Screen Name"
+                        exitcode
+		else
+			echo "Loading EA7KDO $scn Screen Package"
+                fi
+fi
+
+#echo " End Processing Parameters  - $scn $call"
 
 #Start Duration Timer
 start=$(date +%s.%N)
 
+#Disable all command feedback
+if [ ! "$fb" ]; then
+	exec 3>&2
+	exec 2> /dev/null 
+fi
+
 model="$scn"
 tft='.tft' 
-
+#gz='.gz'
 #Put Pi-Star file system in RW mode
-sudo mount -o remount,rw / > /dev/null
+sudo mount -o remount,rw /
 sleep 1s
 
 #Stop the cron service
 sudo systemctl stop cron.service  > /dev/null
 
+# Using "$homedir"/Nextion_Temp/
 
-#Test for "$homedir"/Nextion_Temp and remove it, if it exists
 
-if [ -d "$homedir"/Nextion_Temp ]; then
-  	sudo rm -R "$homedir"/Nextion_Temp
-fi
-
-  # Get Nextion Screen/Scripts and support files from github
-  # Get VE3ZRD File Set
-
-getVE3ZRD
+getea7kdo
  
-
-
-if [ ! -d /usr/local/etc/Nextion_Support ]; then
-	sudo mkdir /usr/local/etc/Nextion_Support
-else
-       sudo rm -R /usr/local/etc/Nextion_Support
-	sudo mkdir /usr/local/etc/Nextion_Support
-fi
-
-sudo chmod +x "$homedir"/Nextion_Temp/*.sh
-sudo rsync -avqru "$homedir"/Nextion_Temp/* /usr/local/etc/Nextion_Support/ --exclude=NX* --exclude=profiles.txt
-
-sudo rsync -avqru "$homedir"/Scripts/stripped2.csv  /usr/local/etc/
-sudo mount -o remount,rw / 
-sudo wget https://database.radioid.net/static/user.csv -O /usr/local/etc/stripped.csv
-
-if [ -f "$homedir"/Nextion_Temp/profiles.txt ]; then
-	if [ ! -f /usr/local/etc/Nextion_Support/profiles.txt ]; then
-        	if [ "$fb" ]; then
-			txtn= "Replacing Missing Profiles.txt"
-			txt="$txt\n""$txtn"
-        	fi
-        	sudo cp  "$homedir"/Nextion_Temp/profiles.txt /usr/local/etc/Nextion_Support/
-	fi
-fi
-
 model="$scn"
-    echo "Remove Existing $model$tft and copy in the new one"
-txtn="Remove Existing $model$tft and copy in the new one"
-txt="$txt""$txtn"
 
-if [ -f /usr/local/etc/"$model$tft" ]; then
-	sudo rm /usr/local/etc/NX*K*.tft
-fi
-sudo cp "$homedir"/Nextion_Temp/"$model$tft" /usr/local/etc/
 
 
  FILE=/usr/local/etc/"$model$tft"
  if [ ! -f "$FILE" ]; then
         # Copy failed
-      	echo "No TFT File Available to Flash - Try Again"
+      echo "No TFT File Available to Flash - Try Again"
 	errtext="Missing tft File Parameter"
 	exitcode
  fi
@@ -171,18 +167,15 @@ sudo cp "$homedir"/Nextion_Temp/"$model$tft" /usr/local/etc/
 sudo systemctl start cron.service  > /dev/null
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
-execution_time=`printf "%.2f seconds" $duration`
+execution_time=`printf "%.2f secs" $duration`
+
+if [ ! "$fb" ]; then
+ exec 2>&3
+fi 
 
 
-txt="$calltxt Scripts Loaded: $execution_time"
-#whiptail --title "$title" --msgbox "$txt" 8 90
-dialog --title "  $title  " --ascii-lines --msgbox "$txt" 8 78
+# echo "$scn Ready  $execution_time"
+echo "$scn Ready to Flash! $execution_time"
 
-echo -e '\e[1;40m'
 
-if [ -z "$1" ]; then
-	clear
-fi
 
-sudo mount -o remount,ro /
-exit
