eebc6bab7eba5fe88b2ed4ea9450c7c67e92b9df
root <root@pi-star>
2024-12-24 19:26:13 -0500

    update

--- gitcopy32.sh
+++ gitcopy32.sh
@@ -9,18 +9,6 @@
 #							#
 #  KF6S/VE3RD                               2024-12-21  #
 #########################################################
-# Valid Screen Names for VE3ZRD NX4024K032
-
-#if [[ $EUID -ne 0 ]]; then
-#	clear
-#	echo ""
- #  	echo "This script must be run as root"
-#	echo "Setting root user"
-#	echo "Re-Start Script"
-#	echo ""
-#	sudo su
-  # 	exit 1
-#fi
 p1=$(pwd) ; cd .. ; homedir=$(pwd) ; cd "$p1"
 
 who=$(whoami)
@@ -30,12 +18,6 @@ sleep 2
 run=""
 
 errtext="This is a test"
-
-
-parm1="$1"
-parm2="$2"
-ver="20240121"
-declare -i tst
 
 export NCURSES_NO_UTF8_ACS=1
 export LANG=en_US.UTF-8
@@ -51,14 +33,6 @@ sudo sed -i '/screen_color = /c\screen_color = (WHITE,BLUE,ON)' ~/.dialogrc
 sudo sed -i '/title_color = /c\title_color = (YELLOW,RED,ON)' ~/.dialogrc
 echo -e '\e[1;44m'
 
-if [ -z "$1" ]; then
-	clear
-fi
-
-function UpdateNextionDriver
-{
-/home/pi-star/Scripts/NextionDriver4M17.sh
-}
 
 function exitcode
 {
@@ -79,52 +53,33 @@ exit
 # VE3ZRD Script Function
 function getve3zrd
 {
-	tst=0
-#	echo "Function VE3ZRD"
-	calltxt="VE3ZRD"
 
-if [ -d "$homedir"/Nextion_Temp ]; then
-  	sudo rm -R "$homedir"/Nextion_Temp
-fi
+	if [ -d /home/pi-star/Nextion_Temp ]; then
+  		sudo rm -R /home/pi-star/Nextion_Temp
+	fi
+	if [ -d /usr/local/etc/Nextion_Support ]; then
+  		sudo rm  /usr/local/etc/Nextion_Support/*
+	else
+		sudo mkdir /usr/local/etc/Nextion_Support
+	fi
 
-    	if [ "$scn" == "NX4024K032" ]; then
-#	  	sudo git clone --depth 1 https://github.com/TGIF-Network/NX4024K032 "$homedir"/Nextion_Temp
-	  	sudo git clone --depth 1 https://github.com/TGIF-Network/N4024K032 "$homedir"/Nextion_Temp
-		# https://github.com/TGIF-Network/N4024K032
-		tst=1
+	if [ -f /usr/local/etc/*.tft ]; then
+  		sudo rm /usr/local/etc/*.tft
+	fi
+       
+	sudo git clone --depth 1 https://github.com/TGIF-Network/NX4024K032 /home/pi-star/Nextion_Temp
+	rsync /home/pi-star/Nextion_Temp/* /usr/local/etc/Nextion_Support/* 
+	cp /home/pi-star/Nextion_Temp/NX* /usr/local/etc/
+
 		echo "Getting Github repository: https://github.com/TGIF-Network/NX4024K032"
-	fi     
 
 	
 }
 
-function getcall
-{
-#Set Screen Author
-calltxt="VE3ZRD"
-}
 
 #### Start of Main Code
 
 ## Select User Screens
-getcall
-S1=""
-S2=""
-if [ -f "/usr/local/etc/NX4024K032.tft" ]; then
-   S1="NX4024K032"
-   S1A=" Available     "
-else 
-   S1="NX4014K032"
-   S1A=" Not Available - But will Download it "
-fi
-
-
-scn="NX4024K032"
-
-echo "$scn $calltxt"
-
-
-#echo " End Processing Parameters  - $scn $calltxt"
 
 #Start Duration Timer
 start=$(date +%s.%N)
@@ -138,61 +93,18 @@ sleep 1s
 
 #Stop the cron service
 sudo systemctl stop cron.service  > /dev/null
-
-
-#Test for "$homedir"/Nextion_Temp and remove it, if it exists
-
-if [ -d "$homedir"/Nextion_Temp ]; then
-  	sudo rm -R "$homedir"/Nextion_Temp
-fi
-
-  # Get Nextion Screen/Scripts and support files from github
-  # Get EA7KDO File Set
-
-	echo "getting Screens for $calltxt"
-	getve3zrd
- 
-
-if [ ! -d /usr/local/etc/Nextion_Support ]; then
-	sudo mkdir /usr/local/etc/Nextion_Support
-else
-       sudo rm -R /usr/local/etc/Nextion_Support
-	sudo mkdir /usr/local/etc/Nextion_Support
-fi
-
 sudo chmod +x "$homedir"/Nextion_Temp/*.sh
-sudo rsync -avqru "$homedir"/Nextion_Temp/* /usr/local/etc/Nextion_Support/ --exclude=NX* --exclude=profiles.txt
 
 sudo rsync -avqru "$homedir"/Scripts/stripped2.csv  /usr/local/etc/
 sudo mount -o remount,rw / 
 sudo wget https://database.radioid.net/static/user.csv -O /usr/local/etc/stripped.csv
 
-if [ -f "$homedir"/Nextion_Temp/profiles.txt ]; then
-	if [ ! -f /usr/local/etc/Nextion_Support/profiles.txt ]; then
-        	if [ "$fb" ]; then
-			txtn= "Replacing Missing Profiles.txt"
-			txt="$txt\n""$txtn"
-        	fi
-        	sudo cp  "$homedir"/Nextion_Temp/profiles.txt /usr/local/etc/Nextion_Support/
-	fi
-fi
-
-model="$scn"
-    echo "Remove Existing $model$tft and copy in the new one"
-txtn="Remove Existing $model$tft and copy in the new one"
-txt="$txt""$txtn"
-
-if [ -f /usr/local/etc/"$model$tft" ]; then
-	sudo rm /usr/local/etc/NX*K*.tft
-fi
-sudo cp "$homedir"/Nextion_Temp/"$model$tft" /usr/local/etc/
-
 if [ ! -f "/etc/Colors.ini" ]; then
   cp /home/pi-star/Nextion_Support/Colors.ini /etc/
 fi
 
- FILE=/usr/local/etc/"$model$tft"
- if [ ! -f "$FILE" ]; then
+ FILE="/usr/local/etc/*.tft"
+ if [ ! -f /usr/local/etc/*.tft ]; then
         # Copy failed
       	echo "No TFT File Available to Flash - Try Again"
 	errtext="Missing tft File Parameter"
@@ -200,8 +112,6 @@ fi
  fi
 
 sudo systemctl start cron.service  > /dev/null
-
-UpdateNextionDriver
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
 execution_time=`printf "%.2f seconds" $duration`
